a21ff5016439e2c3d99a58c094cf5208da389fbd
Lucca Couto Barberato <lucca.barberato@gmail.com>
2023-11-11 14:10:20 -0300

    fix: docstring F1

--- features.py
+++ features.py
@@ -2,13 +2,13 @@ def F1(seq: str) -> int:
     '''
     Feature 1: Caractere mais frequente
     
-    Função que retorna o caractere mais frequente (0 ou 1)
+    Função que retorna o caractere mais frequente (0 ou 1) ou -1 se a frequência for igual
     
     Args: 
         seq (str): sequência
         
     Returns:
-        int: 0 ou 1
+        int: -1, 0 ou 1
     '''
     
     return -1 if seq.count('0') == seq.count('1') else int(max('01', key=seq.count))
