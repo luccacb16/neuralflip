7edcb86aa314f7d5a129f5c8070949dc6076568d
Lucca Couto Barberato <lucca.barberato@gmail.com>
2023-11-14 15:30:21 -0300

    F15: Complexidade de Lempel-Ziv

--- features.py
+++ features.py
@@ -149,3 +149,31 @@ def F10(seq: str) -> int:
     '''
     
     return seq.count('11111')
+
+def F15(seq: str) -> int:
+    '''
+    Feature 15: Complexidade de Lempel-Ziv
+    
+    Função que retorna a complexidade de Lempel-Ziv da sequência.
+    Esta medida reflete a quantidade e a variedade de padrões na sequência.
+    
+    Args: 
+        seq (str): sequência de caracteres '0' e '1'.
+        
+    Returns:
+        int: valor da complexidade de Lempel-Ziv.
+    '''
+
+    n = len(seq)
+    i, complexidade_lz = 0, 0
+    substrings = set()
+
+    while i < n:
+        nova_substring = ''
+        while i < n and (nova_substring in substrings or nova_substring == ''):
+            nova_substring += seq[i]
+            i += 1
+        substrings.add(nova_substring)
+        complexidade_lz += 1
+
+    return complexidade_lz
